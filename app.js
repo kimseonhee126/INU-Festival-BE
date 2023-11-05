601f6a0cc6debb518bf4bfd143e875a603111031
Rundo <rundo999@gmail.com>
2023-11-06 00:58:07 +0900

    부스 데이터 추가, top10출력

--- app.js
+++ app.js
@@ -43,52 +43,42 @@ app.get('/lineup', async(req, res) => {
         });
     
         res.send(celebPerform);
-      }
-      catch (err) {
+    }
+    catch (err) {
         console.error('데이터를 가져오는 중 오류 발생:', err);
         res.status(500).json({ error: '데이터를 불러올 수 없습니다.' });
-      }
+    }
 });
 
 // 메인페이지 - 부스 랭킹 Top 5
 app.get('/ranking', async (req, res) => {
     try {
-        const allBooths = await Booth.findAll();
-        
-        const filteredBooths = [];
+        const allBooths = await Booth.findAll({
+            attributes: ['id', 'name', 'category', 'department', 'description', 'liked'],
+            order: [['liked', 'DESC']], // liked 속성을 기준으로 내림차순으로 정렬
+            limit: 10, // 상위 10개 결과만 반환
+        });
 
-        for (let i = 0; i < allBooths.length; i++) {
-            const boothId = allBooths[i].id;
+        const Booths = await Promise.all(allBooths.map(async (booth) => {
+            const boothId = booth.id;
 
             const myBoothDays = await BoothDay.findAll({
                 where: { boothId: boothId },
+                attributes: ['id', 'day', 'time'],
             });
 
-            const selectedKeys = ['id', 'day', 'time'];
+            return {
+                ...booth.get({ plain: true }),
+                boothDays: myBoothDays.map(day => day.get({ plain: true })),
+            };
+        }));
 
-            const filteredBoothDays = myBoothDays.map((item) => {
-                const selectedObject = {};
-                selectedKeys.forEach((key) => {
-                    selectedObject[key] = item[key];
-                });
-                return selectedObject;
-            });
-
-            const selectedKeys2 = ['id', 'name', 'category', 'department', 'description', 'liked'];
-
-            const selectedObject2 = {};
-            selectedKeys2.forEach((key) => {
-                selectedObject2[key] = allBooths[i][key];
-            });
-
-            selectedObject2.boothDays = filteredBoothDays;
-            filteredBooths[i] = selectedObject2;
-        }
-        res.send({ booths: filteredBooths });
+        res.send({ booths: Booths });
     } catch (err) {
         console.error('ERROR: ', err);
     }
 });
+
 
 // 메인 페이지 - 한 줄 외치기 
 // 코드 없습니다
@@ -116,11 +106,11 @@ app.get('/perform', async(req, res) => {
         });
     
         res.send(studentPerform);
-      }
-      catch (err) {
+    }
+    catch (err) {
         console.error('데이터를 가져오는 중 오류 발생:', err);
         res.status(500).json({ error: '데이터를 불러올 수 없습니다.' });
-      }
+    }
 });
 
 // Running the Server: 포트번호는 5000
