2fa106b049f6261f5c912fc4527df612682a58a4
Rundo <rundo999@gmail.com>
2023-11-29 18:49:10 +0900

    booth, boothdays seed 리셋시 id 다시 1부터 시작되도록 수정

--- seeders/2seed-BoothDays.js
+++ seeders/2seed-BoothDays.js
@@ -3,6 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+
+    // id를 1부터 다시 시작하는 로직 추가
+    await queryInterface.sequelize.query('ALTER TABLE Booths AUTO_INCREMENT = 1;');
+
     // foreign key constraint를 위해 booth 테이블의 id를 가져온다.
     const booths = await queryInterface.sequelize.query(`SELECT id from Booths;`);
     const boothRows = booths[0];
@@ -11,22 +15,119 @@ module.exports = {
       'BoothDays',
       [
         {
-          id: 1,
           day: '화',
           time: '13:00 ~ 17:00',
           boothId: boothRows[0].id,
         },
         {
-          id: 2,
           day: '수',
           time: '14:00 ~ 17:00',
           boothId: boothRows[0].id,
         },
         {
-          id: 3,
           day: '목',
           time: '14:00 ~ 17:00',
           boothId: boothRows[1].id,
+        },
+        {
+          day: '화',
+          time: '13:00 ~ 17:00',
+          boothId: boothRows[2].id,
+        },
+        {
+          day: '수',
+          time: '14:00 ~ 17:00',
+          boothId: boothRows[2].id,
+        },
+        {
+          day: '화',
+          time: '13:00 ~ 17:00',
+          boothId: boothRows[3].id,
+        },
+        {
+          day: '수',
+          time: '14:00 ~ 17:00',
+          boothId: boothRows[3].id,
+        },
+        {
+          day: '화',
+          time: '13:00 ~ 17:00',
+          boothId: boothRows[4].id,
+        },
+        {
+          day: '수',
+          time: '14:00 ~ 17:00',
+          boothId: boothRows[4].id,
+        },
+        {
+          day: '화',
+          time: '13:00 ~ 17:00',
+          boothId: boothRows[5].id,
+        },
+        {
+          day: '수',
+          time: '14:00 ~ 17:00',
+          boothId: boothRows[5].id,
+        },
+        {
+          day: '화',
+          time: '13:00 ~ 17:00',
+          boothId: boothRows[6].id,
+        },
+        {
+          day: '수',
+          time: '14:00 ~ 17:00',
+          boothId: boothRows[6].id,
+        },
+        {
+          day: '화',
+          time: '13:00 ~ 17:00',
+          boothId: boothRows[7].id,
+        },
+        {
+          day: '수',
+          time: '14:00 ~ 17:00',
+          boothId: boothRows[7].id,
+        },
+        {
+          day: '화',
+          time: '13:00 ~ 17:00',
+          boothId: boothRows[8].id,
+        },
+        {
+          day: '수',
+          time: '14:00 ~ 17:00',
+          boothId: boothRows[8].id,
+        },
+        {
+          day: '화',
+          time: '13:00 ~ 17:00',
+          boothId: boothRows[9].id,
+        },
+        {
+          day: '수',
+          time: '14:00 ~ 17:00',
+          boothId: boothRows[9].id,
+        },
+        {
+          day: '화',
+          time: '13:00 ~ 17:00',
+          boothId: boothRows[10].id,
+        },
+        {
+          day: '수',
+          time: '14:00 ~ 17:00',
+          boothId: boothRows[10].id,
+        },
+        {
+          day: '화',
+          time: '13:00 ~ 17:00',
+          boothId: boothRows[11].id,
+        },
+        {
+          day: '수',
+          time: '14:00 ~ 17:00',
+          boothId: boothRows[11].id,
         },
       ]
     )
