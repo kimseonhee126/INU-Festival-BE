0023dc1d0e005e7bd2427bab848f6b9fc9d95406
heondo <rundo999@gmail.com>
2024-03-14 13:46:16 +0900

    양방향 but undefined

--- router/oneline/oneline.js
+++ router/oneline/oneline.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const db = require('../../models');
 const fs = require('fs')
 
+// JSON 미들웨어 사용
+router.use(express.json());
+
 router.use(express.urlencoded({ extended: false }));
+
+const { OneLine } = db;     // db.OneLine
+const { User } = db;     // db.User
 
 router.get("/", async (req, res) => {
   fs.readFile("./static/index.html", function (err, data) {
@@ -15,6 +21,51 @@ router.get("/", async (req, res) => {
       res.end();
     }
   });
+});
+
+// 채팅 메시지 불러오기
+router.get('/all_messages', async (req, res) => {
+  const ALLOnelines = await OneLine.findAll({
+    attributes: ['id', 'content', 'emoji', 'userId'],
+  });
+
+  const Onelines = await Promise.all(ALLOnelines.map(async (oneline) => {
+    const user = await User.findOne({ where: { id: oneline.userId } });
+    return {
+      ...oneline.get({ plain: true }),
+      studentId: user.studentId,
+      content: oneline.content,
+      emoji: oneline.emoji
+    };
+  }));
+  res.send(Onelines);
+});
+
+// 채팅 메시지 추가(DB에 저장 하고 보내기)
+router.post("/add", async (req, res) => {
+  const token = req.headers['authorization'];
+  console.log('token:토큰은', token);
+  const tokenValue = token ? token.split(' ')[1] : null;
+  // 토큰으로 유저 찾기
+  const existUser = await User.findOne({ where: { token: tokenValue } });
+  if (!existUser) { // 로그인을 하지 않은 경우
+      return res.status(400).send({ success: false, message: '로그인 먼저 하세요!' });
+  } else {
+    console.log(req.body)
+    const { content, emoji } = req.body;
+    const newOneLine = await OneLine.create({
+      content: content,
+      emoji: emoji,
+      userId: existUser.id
+    });
+
+    const data = {
+      studentId: existUser.studentId,
+      content: newOneLine.content,
+      emoji: newOneLine.emoji
+    }
+    res.status(201).json(data);
+  }
 });
 
 module.exports = router;
--- static/js/index.js
+++ static/js/index.js
@@ -1,38 +1,21 @@
 //여기는 클라이언트 쪽 코드입니다.
+const socket = io()
 
-// /* 접속 되었을 때 실행 */
-// socket.on('connect', function() {
+/* 접속 되었을 때 실행 */
+socket.on('connect', function() {
 
-//   socket.name = studentId
-
-//   /* 서버에 새로운 유저가 왔다고 알림 */
-//   socket.emit('newUser', name)
-// })
+  /* 서버에 새로운 유저가 왔다고 알림 */
+  // socket.emit('newUser', name)
+})
 
 /* 서버로부터 데이터 받은 경우 */
 socket.on('update', function(data) {
   var chat = document.getElementById('chat')
 
   var message = document.createElement('div')
-  var node = document.createTextNode(`${data.name}: ${data.message}`)
-  var className = ''
+  var node = document.createTextNode(`${data.emoji}:${data.studentId}: ${data.content}`)
 
-  // 타입에 따라 적용할 클래스를 다르게 지정
-  switch(data.type) {
-    case 'message':
-      className = 'other'
-      break
-
-    case 'connect':
-      className = 'connect'
-      break
-
-    case 'disconnect':
-      className = 'disconnect'
-      break
-  }
-
-  message.classList.add(className)
+  message.classList.add('me')
   message.appendChild(node)
   chat.appendChild(message)
 })
@@ -40,19 +23,35 @@ socket.on('update', function(data) {
 /* 메시지 전송 함수 */
 function send() {
   // 입력되어있는 데이터 가져오기
-  var message = document.getElementById('test').value
+  var content = document.getElementById('test').value
+  console.log(`메세지는 : ${content}`)
   
   // 가져왔으니 데이터 빈칸으로 변경
   document.getElementById('test').value = ''
 
-  // 내가 전송할 메시지 클라이언트에게 표시
-  var chat = document.getElementById('chat')
-  var msg = document.createElement('div')
-  var node = document.createTextNode(message)
-  msg.classList.add('me')
-  msg.appendChild(node)
-  chat.appendChild(msg)
-
-  // 서버로 message 이벤트 전달 + 데이터와 함께
-  socket.emit('message', {type: 'message', message: message})
+  fetch("http://localhost:4000/shout/add", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${authToken}`,
+    },
+    body: JSON.stringify({
+      content: content,
+      emoji: "happy",
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      // 내가 전송할 메시지 클라이언트에게 표시
+      let chat = document.getElementById('chat')
+      let msg = document.createElement('div')
+      let node = document.createTextNode(`${data.emoji}: ${data.studentId}: ${data.content}`)
+      msg.classList.add('me')
+      msg.appendChild(node)
+      chat.appendChild(msg)
+      // 서버로 message 이벤트 전달 + 데이터와 함께
+      socket.emit('message', {data: data})
+    })
+    .catch((error) => console.error("Error:", error));
 }
