4be8245c347c61e90dda02f6371a275fc2d3f5f8
heondo <rundo999@gmail.com>
2024-03-25 15:39:49 +0900

    Fix: "booth 이미지 seed에 이미지 3개씩 넣기"

--- seeders/3seed-BoothImg.js
+++ seeders/3seed-BoothImg.js
@@ -1,33 +1,36 @@
-"use strict";
+'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up (queryInterface, Sequelize) {
     // id를 1부터 다시 시작하는 로직 추가
-    await queryInterface.sequelize.query(
-      "ALTER TABLE BoothImgs AUTO_INCREMENT = 1;"
-    );
+    await queryInterface.sequelize.query('ALTER TABLE BoothImgs AUTO_INCREMENT = 1;');
 
-    // foreign key constraint를 위해 booth 테이블의 id를 가져온다.
-    const booths = await queryInterface.sequelize.query(
-      `SELECT id from Booths;`
-    );
-    const boothRows = booths[0];
-
-    const boothImgsData = [];
-    for (let i = 0; i < 77; i++) {
-      for (let j = 0; j < 3; j++) { // 각 boothId에 대해 이미지를 3번씩 추가합니다.
-        boothImgsData.push({
-          url: "BOL.jpeg",
-          boothId: boothRows[i].id,
-        });
-      } 
+    const insertData = [];
+    for(let boothId = 1; boothId <= 77; boothId++) {
+        // 각 boothId에 대해 같은 이미지를 3개씩 추가합니다.
+        for(let imgCount = 0; imgCount < 3; imgCount++) {
+            insertData.push({
+                url: 'BOL.jpeg',
+                boothId: boothId,
+                // createdAt, updatedAt 필드도 추가해야 할 수 있습니다. 아래는 예시입니다.
+                createdAt: new Date(),
+                updatedAt: new Date()
+            });
+        }
     }
 
-    await queryInterface.bulkInsert("BoothImgs", boothImgsData);
+    // 생성된 데이터 배열로 bulkInsert를 실행합니다.
+    await queryInterface.bulkInsert('BoothImgs', insertData);
   },
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("BoothImgs", null, {});
-  },
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('BoothImgs', null, {});
+  }
 };
