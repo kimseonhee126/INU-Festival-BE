777d2073fc40ced212ef8630839afccd538ddb70
kimseonhee126 <kimseonhee126@gmail.com>
2024-03-26 01:56:05 +0900

    keyword

--- router/keyword/keyword.js
+++ router/keyword/keyword.js
@@ -11,28 +11,38 @@ const { Keywords } = db;     // db.Keyword
 */
 
 // 임의의 데이터
-const sentences = [
-    '나는 고양이를 좋아합니다.',
-    '오늘은 날씨가 좋네요.',
-    '이것은 테스트 문장입니다.',
-    '커피를 마시면 기분이 좋아집니다.',
-    '바다가 보고 싶어요.',
-    '나는 Node.js를 배우고 있습니다.',
-    '오늘은 무엇을 먹을까요?',
-    '햇살이 따뜻하네요.',
-    '책을 읽으면 많은 것을 배울 수 있어요.',
-    '나는 새로운 언어를 배우고 싶습니다.',
-    '여행을 가면 마음이 행복해집니다.',
-    '피곤할 때는 잠을 자는 것이 좋아요.',
-    '나는 친구들과 함께 시간을 보내는 것을 즐깁니다.',
-    '음악을 듣는 것은 마음의 힐링이 됩니다.',
-    '운동을 하면 몸이 건강해집니다.',
-    '요리를 하면 맛있는 음식을 먹을 수 있어요.',
-    '집에서 영화를 보는 것이 편안합니다.',
-    '문제를 해결하는 것은 머리를 깊게 생각하는 기회입니다.',
-    '꽃을 보면 마음이 환해집니다.',
-    '나는 새로운 경험을 하는 것을 좋아합니다.'
-];
+// const sentences = [
+//     '오늘은 무엇을 먹을까요?',
+//     '햇살이 따뜻하네요.',
+//     '책을 읽으면 많은 것을 배울 수 있어요.',
+//     '나는 새로운 언어를 배우고 싶습니다.',
+//     '여행을 가면 마음이 행복해집니다.',
+//     '피곤할 때는 잠을 자는 것이 좋아요.',
+//     '나는 친구들과 함께 시간을 보내는 것을 즐깁니다.',
+//     '음악을 듣는 것은 마음의 힐링이 됩니다.',
+//     '운동을 하면 몸이 건강해집니다.',
+//     '요리를 하면 맛있는 음식을 먹을 수 있어요.',
+//     '집에서 영화를 보는 것이 편안합니다.',
+//     '문제를 해결하는 것은 머리를 깊게 생각하는 기회입니다.',
+//     '꽃을 보면 마음이 환해집니다.',
+//     '나는 새로운 경험을 하는 것을 좋아합니다.'
+// ];
+
+// 문장리스트
+let sentences = [];
+
+// 클라이언트에서 POST 요청을 보낼 때마다 키워드 업데이트
+router.post('/', async (req, res) => {
+    try {
+        const { sentence } = req.body;
+        sentences.push(sentence);
+        await updateKeywords();
+        res.status(200).json({ sentence });
+    } catch (err) {
+        console.error(`error : ${err}`);
+        res.status(500).json({ err });
+    }
+});
 
 // keyword 추출하기
 function extractKeyword(sentences) {
@@ -87,6 +97,8 @@ setInterval(updateKeywords, interval)
 // 
 router.get('/', async (req, res) => {
     try {
+        const sentence = req.body;
+        sentences.push(sentence);
 
         if (cacheKeyword) {
             let keywords = [];
@@ -104,22 +116,6 @@ router.get('/', async (req, res) => {
         } else {
             res.status(500).json({ error: '키워드가 없습니다...' });
         }
-
-        // const mecabKeyword = await extractKeyword(sentences);
-
-        // let keywords = [];
-
-        // Object.keys(mecabKeyword).forEach((sentence, index) => {
-        //     const id = index + 1;
-        //     const keywordList = mecabKeyword[sentence];
-        //     keywordList.forEach(keyword => {
-        //         keywords.push({ id, keyword });
-        //     });
-        // });
-
-        // console.log(`keywords : ${JSON.stringify(keywords)}`);
-
-        // res.json({ keywords });
     } catch (err) {
         console.error(`error : ${err}`);
         res.status(500).json({ err });
