69e55dc9cdbd55d291e844ad848a75eb4fb3fe8d
heondo <rundo999@gmail.com>
2024-04-12 12:43:17 +0900

    Auto stash before checking out "HEAD"

--- router/manage/manage.js
+++ router/manage/manage.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
-// const db = require('../../models');
+const dotenv = require("dotenv");
+const db = require('../../models');
+
+// .env 파일 사용하기 위해
+dotenv.config();
+
+const { User } = db;
+const { Booth } = db;
+
 const fs = require('fs')
 
 const multer = require('multer');
@@ -10,53 +18,56 @@ const _storage = multer.diskStorage({
     cb(null, 'public/img/')
   },
   filename: function (req, file, cb) {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    const second = String(date.getSeconds()).padStart(2, '0');
+    const formattedDate = `${year}-${month}-${day}-${hour}-${minute}-${second}`;
     const safeName = file.originalname.replace(/[^a-z0-9.]/gi, '').toLowerCase();
-    cb(null, safeName);
+    const nameWithDate = `${formattedDate}-${safeName}`;
+    cb(null, nameWithDate);
   }
 })
 const upload = multer({ storage: _storage })
 
 // JSON 미들웨어 사용
-// router.use(express.json());
+router.use(express.json());
 
-// router.use(express.urlencoded({ extended: false }));
+router.use(express.urlencoded({ extended: false }));
 
 // 로그인 페이지
 router.get("/", async (req, res) => {
-  fs.readFile("./static/login.html", function (err, data) {
-    if (err) {
-      res.send("에러");
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    }
-  });
+  res.render('login');
 });
 
 // 리스트 페이지
-router.get('/list', function(req, res){
-  fs.readFile("./static/list.html", function (err, data) {
-    if (err) {
-      res.send("에러");
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    }
-  });
+router.get('/list', async (req, res) => { 
+  try{
+    // console.log(req.headers)
+    // 토큰 받기
+    const token = req.headers["authorization"];
+    // console.log(`토큰: ${token}`);
+    const tokenValue = token ? token.split(" ")[1] : null;
+    // console.log(`토큰값: ${tokenValue}`);
+
+    // 해당 토큰을 가지고 있는 user 찾기
+    const findUser = await User.findOne({ where: { token: tokenValue } });
+    // console.log(`유저: ${findUser.studentId}`);
+    const booth = await Booth.findOne({ where: { id: findUser.rank }});
+    console.log(`부스: ${booth.name}`);
+
+    res.json({ success: true, booth: booth });
+
+  } catch(err) {
+    res.json({ success: false, message: "서버 내부 오류"  });
+  }
 });
 
 router.get('/detail', function(req, res){
-  fs.readFile("./static/detail.html", function (err, data) {
-    if (err) {
-      res.send("에러");
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    }
-  });
+  res.render('detail');
 });
 router.post('/detail', upload.single('public/img'), function(req, res){
   console.log(req.file);
