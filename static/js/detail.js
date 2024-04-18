63c9532b6e6166cf30d37f613b81b0f090f18641
heondo <rundo999@gmail.com>
2024-04-19 01:56:07 +0900

    `${myUrl}/manage/detail`

--- static/js/detail.js
+++ static/js/detail.js
@@ -110,7 +110,7 @@ function submitForm() {
   });
 
   // 함수 호출
-  axiosWithAuthForUpload('detail', formData)
+  axiosWithAuthForUpload(`${myUrl}/manage/detail`, formData)
     .then(data => {
       // 성공 후 처리, 예를 들어 페이지 리다이렉트
       window.location.href = '/manage';
