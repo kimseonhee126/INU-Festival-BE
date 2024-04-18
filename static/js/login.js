d3e1be51f5cf560d45e4b0718638b65f1ae92ae1
heondo <rundo999@gmail.com>
2024-04-19 00:43:30 +0900

    Fix: "login.js url수정"

--- static/js/login.js
+++ static/js/login.js
@@ -21,7 +21,7 @@ function fetchWithAuth(url, options = {}) {
 }
 
 // const myUrl = "http://localhost:4000"; // -> 개발용
-const myUrl = "3.36.49.113.nip.io"; // -> 배포용
+const myUrl = "https://13.125.142.74.nip.io/"; // -> 배포용
 
 // 즉시 실행 함수 -> user/me API 호출 -> 자동로그인
 (function() {
